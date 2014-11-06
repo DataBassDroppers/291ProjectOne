a6c38266cd35331aca7f55fc1d0cc4994f0dc85f
Peter Crinklaw <crinklaw@cs.ualberta.ca>
2014-11-05 17:49:22 -0700

    Added creditial file handler for convenience

--- Prescription.py
+++ Prescription.py
@@ -1,6 +1,5 @@
 import cx_Oracle
-
-
+#import getpass #gets password without echoing
 
 class Prescription():
 
@@ -8,13 +7,17 @@ class Prescription():
     #    Prescription().run()
         
     def __main__():
-        user = input('Username: ')
-        password = input('Password: ')
         
-
-
-        con = cx_Oracle.connect(user + '/' + \
-                                password + '@gwynne.cs.ualberta.ca:1521:CRS')
+        #password = getpass.getpass() # could use this to get password, but doesn't work with IDE
+        
+        f = open('credentials', 'r')
+        
+        username = f.readline()[:-1] #[-1] to trim \n
+        password = f.readline()[:-1]
+        
+        
+        con = cx_Oracle.connect(username + '/' + \
+                                password + '@gwynne.cs.ualberta.ca:1521/CRS')
         
         
 
