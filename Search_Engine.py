87b904c074efc099ae98fa475958cf75a56569f8
pietrasi <pietrasi@ualberta.ca>
2014-11-10 20:52:07 -0700

    dont know why the prescription stuff wasnt included

--- Search_Engine.py
+++ Search_Engine.py
@@ -56,3 +56,122 @@ class SearchEngine():
                 counter+=1
                 list1.append(x)
             print(tuple(list1))
+    def goodNumber(self,string,case):
+        if case == "D":
+            curs = self.con.cursor()
+            curs.execute("select * from doctor where employee_no like'"+string+"'")
+            rows = curs.fetchall()
+            if len(rows) == 0:
+                return False
+            else:
+                return True
+        else:
+            curs = self.con.cursor()
+            curs.execute("select * from patient where health_care_no like'"+string+"'")
+            rows = curs.fetchall()
+            if len(rows) == 0:
+                return False
+            else:
+                return True
+        
+    def isReal(self,string,case):
+        if case == "D":
+            curs = self.con.cursor()
+            curs.execute("select * from doctor d, patient p where d.health_care_no=p.health_care_no and p.name like'"+string+"'")
+            rows = curs.fetchall()
+            if len(rows) == 0:
+                return False
+            else:
+                return True
+        elif case == "T":
+            curs = self.con.cursor()
+            curs.execute("select * from test_type where test_name like'"+string+"'")
+            rows = curs.fetchall()
+            if len(rows) == 0:
+                return False
+            else:
+                return True
+        else:
+            curs = self.con.cursor()
+            curs.execute("select * from patient where name like'"+string+"'")
+            rows = curs.fetchall()
+            if len(rows) == 0:
+                return False
+            else:
+                return True
+        
+    def getDoctorNumber(self,string):
+        
+     
+        curs = self.con.cursor()
+        curs.execute("select employee_no from doctor d,patient p where p.name like '"+string+"' and p.health_care_no=d.health_care_no")
+        rows = curs.fetchall()
+        
+        for row in rows:
+            id1=int(row[0])
+        print(string,"employee id is",id1)
+        return id1
+    
+    
+    def isNumber(self, string):
+        return string.isdigit()
+    
+
+    def getTestName(self):
+    
+        curs = self.con.cursor()
+        curs.execute('select test_name from test_type')
+        rows = curs.fetchall()
+        for row in rows:
+            print(row)
+            
+        string = input('Enter test name: ')
+        if self.isReal(string,"T"):
+            return string,False 
+        else:
+            print("Not a real test, please try again")
+            return False, True
+    
+    def getPatient(self):
+        curs = self.con.cursor()
+
+        curs.execute("select name,health_care_no from patient p")
+
+        rows = curs.fetchall()
+        
+        for row in rows:
+            print(row)
+        
+        string = input('Enter Patient name or number: ')
+        
+        if self.isNumber(string):
+            if self.goodNumber(string,"P"):
+                print("patient health care number is",int(string))
+                return int(string),False
+            else:
+                print("Invalid health care number")
+                return False,True
+        else:
+            if self.isReal(string,"P"):
+                return self.getPatientNumber(string),False
+            else:
+                print(string,"is not a real patient, try again")
+                return False,True
+            
+    def getPatientNumber(self,string):
+        curs = self.con.cursor()
+
+        curs.execute("select health_care_no from patient p where p.name like '"+string+"'")
+
+        rows = curs.fetchall()
+        
+        for row in rows:
+            id1=int(row[0])
+        print(string,"employee id is",id1)
+        return id1
+        
+    
+    def printSeparator(self):
+        print("")
+        print("-----------------------")
+        print("")
